24cc4b862b5421ed936b5b50a50ad6061bd4c0cf
Shohaib <shohaibpt@gmail.com>
2026-04-29 20:08:01 +0400

    deleted unwanted folders

--- Chatbot_Web/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
-  styles: [':host { display: block; height: 100%; }']
-})
-export class AppComponent {}
